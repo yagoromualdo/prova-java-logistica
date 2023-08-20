1be527b2f6fd17a90b65993bafab304ea4fd4b58
Yago Romualdo <yagoromualdo.yr@gmail.com>
2023-08-19 23:02:33 -0300

    Implementando documentação da API

--- back-end/src/main/java/com/logistica/api/controller/ClientContoller.java
+++ back-end/src/main/java/com/logistica/api/controller/ClientContoller.java
@@ -15,8 +15,12 @@ import com.logistica.api.core.controller.ResponseAbstractController;
 import com.logistica.api.entity.Client;
 import com.logistica.api.service.ClientService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/client")
+@Tag(name = "Cliente/Client", description = "Cadastro manual externo")
 public class ClientContoller extends ResponseAbstractController {
 
     private ClientService clientService;
@@ -25,26 +29,31 @@ public class ClientContoller extends ResponseAbstractController {
         this.clientService = clientService;
     }
 
+    @Operation(description = "Retorna uma lista de clientes")
     @GetMapping
     public ResponseEntity<?> findAll() {
         return jsonResponse(clientService.findAll());
     }
 
+    @Operation(description = "Busca um cliente pelo id")
     @GetMapping("{id}")
     public ResponseEntity<?> findById(@PathVariable long id) {
         return jsonResponse(clientService.findById(id));
     }
 
+    @Operation(description = "Salva um novo cliente")
     @PostMapping
     public ResponseEntity<?> save(@Validated @RequestBody Client client) {
         return jsonResponse(clientService.create(client));
     }
 
+    @Operation(description = "Altera um cliente já existente")
     @PutMapping
     public ResponseEntity<?> update(@Validated @RequestBody Client client) {
         return jsonResponse(clientService.update(client));
     }
 
+    @Operation(description = "Exclui um cliente de acordo com o id")
     @DeleteMapping("{id}")
     public ResponseEntity<?> deleteById(@PathVariable long id) {
         return jsonResponse(clientService.findById(id));
