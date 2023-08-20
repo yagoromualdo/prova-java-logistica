44b1a6ae6b5bc8ecb5b362ae6cb45e930a98aa6f
Yago Romualdo <yagoromualdo.yr@gmail.com>
2023-08-20 15:59:33 -0300

    Criando rotas iniciais

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientListComponent } from './components/client/client-list/client-list.component';
+import { ClientFormComponent } from './components/client/client-form/client-form.component';
+import { ClientMapsComponent } from './components/client/client-maps/client-maps.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'client',
+    pathMatch: 'full'
+  },
+  {
+    path: 'client',
+    component: ClientListComponent
+  },
+  {
+    path: 'client/create',
+    component: ClientFormComponent
+  },
+  {
+    path: 'client/view/:idClient',
+    component: ClientFormComponent
+  },
+  {
+    path: 'client/update/:idClient',
+    component: ClientFormComponent
+  },
+  {
+    path: 'client/maps',
+    component: ClientMapsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
