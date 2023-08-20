c493834c02c36feeaabcc7cf74e971383f59df5a
Yago Romualdo <yagoromualdo.yr@gmail.com>
2023-08-20 16:11:45 -0300

    Adicionando o bootstrap na aplicação

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientListComponent } from './components/client/client-list/client-list.component';
+import { ClientFormComponent } from './components/client/client-form/client-form.component';
+import { ClientMapsComponent } from './components/client/client-maps/client-maps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    ClientFormComponent,
+    ClientMapsComponent
   ],
   imports: [
     BrowserModule,
